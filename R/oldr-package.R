fdcf0643e3b70961e1f1ea9e2847aa0f16b3ff1e
JB Duck-Mayr <j.duckmayr@gmail.com>
2018-08-03 09:43:41 -0500

    Add package documentation, handle imports from utils

---
+++ R/oldr-package.R
@@ -0,0 +1,10 @@
+#' oldr
+#'
+#' oldr provides a function to install the latest available version of an R
+#' package for old versions of R.
+#'
+#' @name oldr
+#' @docType package
+#' @author  JB Duck-Mayr
+#' @importFrom utils available.packages download.file install.packages sessionInfo untar
+NULL
